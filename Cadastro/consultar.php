1f982686f99f2050a4841d11b4f0de25bd594d5f
Pablo Santos <99838818+P4BLOll@users.noreply.github.com>
2023-05-22 16:35:37 -0300

    Add files via upload

--- Cadastro/consultar.php
+++ Cadastro/consultar.php
@@ -96,6 +96,7 @@ $conexao = null;
                 <th scope="col">CEP</th>
                 <th scope="col">Cidade</th>
                 <th scope="col">Estado</th>
+                <th scope="col"></th>
                 </tr>
             </thead>
             <tbody>
@@ -103,12 +104,20 @@ $conexao = null;
             for($i = 0; $i< count($rows); $i++){?>
                 <tr>
                 <th scope="row"><?php echo $rows[$i]['id'] ?></th>
-                <td><?php echo $rows[$i]['nome'] ?></td>
+                <td><?php echo $rows[$i]['nome'] ?> </td>
                 <td><?php echo $rows[$i]['endereco'] ?></td>
                 <td><?php echo $rows[$i]['bairro'] ?></td>
                 <td><?php echo $rows[$i]['cep'] ?></td>
                 <td><?php echo $rows[$i]['cidade'] ?></td>
                 <td><?php echo $rows[$i]['estado'] ?></td>
+                <td><button type="button" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                    </svg></button>
+                    <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                    </svg></button>
+                </td>
                 </tr>
        <?php }?>
             </tbody>
